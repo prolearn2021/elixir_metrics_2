fbe6cab421c4cb5a42c20146af49ed59183c8ab1
Anastasiia Orlovska <aorlovska@AORLOVSKA-MBP.local>
2025-09-24 16:38:48 +0200

    1

--- mix.exs
+++ mix.exs
@@ -1,8 +1,49 @@
 # mix.exs
-defp deps do
-  [
-    {:phoenix, "~> 1.7"},
-    # ...
-    {:statix, "~> 1.4"}
-  ]
+defmodule MetricsDemo.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :metrics_demo,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  def application do
+    [
+      mod: {MetricsDemo.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.7"},
+      {:phoenix_html, "~> 4.0"},
+      {:phoenix_live_reload, "~> 1.5", only: :dev},
+      {:phoenix_live_view, "~> 0.20"},
+      {:floki, ">= 0.34.0", only: :test},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
+      {:statix, "~> 1.4"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"]
+    ]
+  end
 end
